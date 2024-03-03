e51f3377c8b92b60f6717ff9472ba799b37bef68
risingtechnix <risingtechnix@gmail.com>
2024-03-03 13:22:27 +0530

    Update logo and add arrow icon to contact button

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -10,7 +10,11 @@ const Navbar = () => {
 					{/* wrapper for  the logo and branding */}
 					<div className="flex justify-between items-center gap-[16px]">
 						<Link to="/">
-							<img src="src\assets\Logo.png" alt="logo" />
+							<img
+								src="src\assets\Rising-technix.png"
+								alt="logo"
+								className="h-[55px] w-[55px] rounded-full"
+							/>
 						</Link>
 						<Link to="/">
 							<h1 className=" text-[28px] font-bold">RisingTechnix</h1>
@@ -36,8 +40,9 @@ const Navbar = () => {
 							</li>
 						</ul>
 					</div>
-					<div className="flex justify-center items-center w-[163px] h-[56px] bg-gradient-to-r from-purple-700 to-purple-500 hover:from-purple-500 hover:to-purple-700 text-white rounded-[40px] mr-3">
+					<div className="flex justify-center items-center gap-2 w-[163px] h-[56px] bg-gradient-to-r from-purple-700 to-purple-500 hover:from-purple-500 hover:to-purple-700 text-white rounded-[40px] mr-3">
 						<Link to="/contact">Let's Talk</Link>
+						<img src="src\assets\arrow-up-right.png" alt="" />
 					</div>
 				</div>
 			</nav>
