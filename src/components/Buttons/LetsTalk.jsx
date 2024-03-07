e36f58522ca3d78f83a8440db42b76c4188674bf
risingtechnix <risingtechnix@gmail.com>
2024-03-07 23:03:30 +0530

    Update LetsTalk button styling and add new images and content to Home component

--- src/components/Buttons/LetsTalk.jsx
+++ src/components/Buttons/LetsTalk.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import {Link} from "react-router-dom"
 const LetsTalk = (classes) => {
 	return (
-		<div className="hidden px-4 py-2 bg-gradient-to-r from-purple-700 to-purple-500 hover:from-purple-500 hover:to-purple-700 rounded-full lg:w-[140px] lg:h-[56px] lg:flex lg:justify-between lg:items-center lg:gap-2 font-sora">
+		<div className="hidden px-4 py-2 bg-gradient-to-r from-purple-700 to-purple-500 hover:from-purple-500 hover:to-purple-700 rounded-full lg:w-[140px] lg:h-[56px] lg:flex lg:justify-between lg:items-center lg:gap-2 font-sora transition-all duration-300 ease-in-out">
 			<Link to="/contact" className="text-white">
 				Let's Talk
 			</Link>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 const Home = () => {
-
 	return (
 		<>
 			<div>
@@ -9,26 +9,69 @@ const Home = () => {
 					<img
 						src="src\assets\Background.png"
 						alt="background-image"
-						className="absolute top-0 -z-10"
+						className="absolute top-0 -z-10 h-[90%] md:h-[60vh] lg:h-auto"
 					/>
 				</div>
-				<div className="flex ">
-					<div>
-						<h1>Trendy's Collection</h1>
-						<h1>Building a better you</h1>
-						<p>
-							Every pleasure is to be welcomed and every pain avoided. and every
-							pain avoided. certain circumstances ands
+				<div className="flex flex-col justify-between items-center gap-6 md:flex-row lg:flex-row m-4 px-4 py-8 font-sora">
+					<div className="flex flex-col leading-10 lg:leading-[90px] lg:ml-[64px]">
+						<h1 className="text-[10px] text-[#5B5B5B] lg:text-[17px]">
+							Trendy's Collection
+						</h1>
+						<h1 className="font-bold text-[33px] lg:text-[90px]">
+							Building <br />a better you
+						</h1>
+						<p className="font-[500] text-[14px] leading-7 lg:text-[18px]">
+							Every pleasure is to be welcomed and every pain avoided.
+							<br />
+							and every pain avoided. certain circumstances ands
 						</p>
+						<div className="flex items-center justify-center w-[163px] h-[44px] border-solid border-[2px] mt-6 border-black rounded-[40px] text-center hover:border-none hover:bg-black hover:text-white transition-all duration-300 ease-in-out">
+							<Link to="/about">Learn More</Link>
+						</div>
 					</div>
 					<div>
-						<img src="src\assets\Web-Developmet-10 3.png" alt="" />
+						<img
+							src="src\assets\Desktop_image2.png"
+							alt="image"
+							className="h-[232px] w-[317px] lg:h-[407px] lg:w-[556px] lg:pr-[32px]"
+						/>
+					</div>
+				</div>
+
+				<div>
+					<div className="flex flex-col items-center gap-5 w-[324.72px] bg-black rounded-lg text-white">
+						<h1>Trusted By Clients</h1>
+						<img
+							src="src\assets\div.chy-trusted-1-video.png"
+							alt="video_thumbnail"
+						/>
+						<div>
+							<div>
+								<i class="uis uis-star"></i>
+							</div>
+							<h1>Rated 4.9/5 From over 900 Reviews</h1>
+						</div>
+						<div>
+							<div>
+								<h1>Monthly Traffic</h1>
+							</div>
+							<div>
+								<h1>7.9K</h1>
+								<input
+									type="range"
+									min="0"
+									max="100"
+									step="1"
+									value="70"
+									className="w-60 h-4 rounded-md bg-gray-300 cursor-pointer "
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</>
 	)
-
 }
 
 export default Home
